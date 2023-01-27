127a47803a9eb6aad01ffe624a09268a73bdf8d9
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-27 21:23:28 +0700

    feat: start connection

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,19 @@
-console.log('Hello Typescript');
+import { createConnection } from '~/connection';
+import { PingCommand } from '~/commands';
+import { Router } from '~/router';
+
+async function start() {
+  const conn = await createConnection();
+  const router = new Router();
+
+  const pingCommand = new PingCommand(conn);
+
+  router.register(pingCommand);
+
+  conn.ev.on('messages.upsert', ({ messages }) => {
+    const context = messages[0];
+    router.observe(context);
+  });
+}
+
+start();
