88ce13b45f6cb729b4867c5436a742b14bcd15f3
edwintantawi <edwintantawi.workspace@gmail.com>
2023-01-28 22:04:47 +0700

    feat: add reminder feature

--- src/commands/index.ts
+++ src/commands/index.ts
@@ -2,3 +2,4 @@ export * from './command';
 export * from './ping';
 export * from './chat-ai';
 export * from './image-ai';
+export * from './reminder';
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import 'dotenv/config';
 
 import { createConnection } from '~/connection';
-import { PingCommand, ChatAICommand, ImageAICommand } from '~/commands';
+import { PingCommand, ChatAICommand, ImageAICommand, ReminderCommand } from '~/commands';
 import { Router } from '~/router';
 
 async function start() {
@@ -11,10 +11,12 @@ async function start() {
   const pingCommand = new PingCommand(conn);
   const aiCommand = new ChatAICommand(conn);
   const imageCommand = new ImageAICommand(conn);
+  const reminderCommand = new ReminderCommand(conn);
 
   router.register(pingCommand);
   router.register(aiCommand);
   router.register(imageCommand);
+  router.register(reminderCommand);
 
   conn.ev.on('messages.upsert', ({ messages }) => {
     const context = messages[0];
