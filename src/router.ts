274a7b2fc96a67044cefd7bd23895bc370ad9125
edwintantawi <edwintantawi.workspace@gmail.com>
2023-03-26 22:38:16 +0700

    fix: triggered allowed other command to be override with ai

--- src/router.ts
+++ src/router.ts
@@ -19,7 +19,7 @@ export class Router {
     if (!message.isOwner && !config.isActive) return;
 
     for (const command of this.commands) {
-      const triggered = command.trigger && message.subConversation.startsWith(command.trigger);
+      const triggered = !message.command && command.trigger && message.subConversation.startsWith(command.trigger);
       if (command.keywords.includes(message.command ?? '') || triggered) {
         logger.info(
           `[${command.title}]-[${config.prefix}${message.command}]: From "${message.from}", in room: "${message.room}"`
